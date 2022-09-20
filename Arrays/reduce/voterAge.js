ee9922f752d5e0c66f0fb93ad8e323f6227a29dc
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2022-09-20 08:13:17 +0530

    Array Reduce

--- Arrays/reduce/voterAge.js
+++ Arrays/reduce/voterAge.js
@@ -13,27 +13,30 @@ var voters = [
     {name: 'Zack', age: 19, voted: false}
 ];
 
-let initObj={ youngVotes:0,
-              youngPeople:0,
-              midVotes:0,
-              midPeople:0,
-              oldVotes:0,
-              oldPeople:0,
+let initObj={ "youngVotes":0,
+              "youngPeople":0,
+              "midVotes":0,
+              "midPeople":0,
+              "oldVotes":0,
+              "oldPeople":0,
             }
 
+
 let result=voters.reduce((final,current)=>{
-     if(current.age>=18 && current.age <= 25){
-         final["youngPeople"]=final["youngPeople"] + 1
-         current.voted==true ? final.youngVotes=+1 :final.youngVotes
+     if(current.age <= 25){
+         final["youngPeople"]+=1
+         current.voted==true ? final["youngVotes"]+=1 :final["youngVotes"]
      }
-    //  else if(current.age>=26 && current.age <= 35){
-    //     final.midPeople+=1
-    //     current.voted==true ? final.midVotes=+1 :final.midVotes
-    // }
-    else if(current.age>=36 && current.age <= 55){
-        final.oldPeople+=1
-        current.voted==true ? final.oldVotes=+1 :final.oldVotes
+     else if(current.age <= 35){
+        final["midPeople"]+=1
+        current.voted==true ? final["midVotes"]+=1 :final["midVotes"]
     }
+    else if(current.age <= 55){
+        final["oldPeople"]+=1
+        current.voted==true ? final["oldVotes"]+=1 : final["oldVotes"]
+    }
+
+    return final
 },initObj)            
 
-console.log(result.json)
+console.log(result)
